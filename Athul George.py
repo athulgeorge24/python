71f1c026b432df6e4a5b1ca95d55819cef6b989a
athulgeorge24 <athulgeorge2028@cs.sjcetpalai.ac.in>
2024-10-04 16:05:30 +0530

    Add files via upload

---
+++ Athul George.py
@@ -0,0 +1,39 @@
+'''if 6 > 3:
+        print("six is gearter than three")
+if 6 > 3:
+    print("six is gearter than three")'''
+#if 6 > 3:
+#print("six is gearter than three")
+
+'''x = "5"
+y = int(5)
+z = float(5)
+
+print(type(x))
+print(type(y))
+print(type(z))'''
+'''
+'''
+'''
+x=y=z="hello"
+print(x,y,z)'''
+
+'''
+
+x = "Python "
+y = "is "
+z = "great"
+print(x + y + z)
+
+username=input("enter your name:")
+print("Username is "+username)
+'''
+'''
+a = "Hello "
+b = "world"
+c = a + b
+print(c)
+'''
+
+a = "hello world"
+print(a[1:7])
