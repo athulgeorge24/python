338db465bb9d2a085851d1a8b313e1b87faa9c9c
athulgeorge24 <athulgeorge2028@cs.sjcetpalai.ac.in>
2024-10-18 15:57:59 +0530

    Add files via upload

---
+++ operatorpython.py
@@ -0,0 +1,15 @@
+'''
+Author:Athul George
+Date:18/10/24
+Discription:Program that demonstrates the usage of arithmetic, comparison, and logical operators.
+ Perform a few operations and print the results.
+'''
+
+
+num1=int(input("enter number one:"))
+num2=int(input("enter number two:"))
+print("Sum=",num1+num2,",Division=",num1/num2)
+print("Is number one greater than number two?:",num1>num2)
+print("Are number one and number two are equal?:",num1==num2)
+print("Logical AND :",num1>0 and num2>0)
+print("Logical OR :",num1>0 or num2>num1)
