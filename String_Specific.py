5c0605daefeb59f874eeca3292fcdf3e6fcac3ab
athulgeorge24 <athulgeorge2028@cs.sjcetpalai.ac.in>
2024-10-18 20:17:54 +0530

    Add files via upload

---
+++ String_Specific.py
@@ -0,0 +1,6 @@
+"""
+Author:Athul George
+Date:18/10/2024
+Programme to print a string in specific series
+"""
+print("Twinkle, twinkle,little star,\n\tHow i wonder what you are!,\n\t\t Up above the world so high,\n\t\t Like diamond in the sky,\nTwinkle twinkle little star,\n\tHow i wonder what yoy are" )
