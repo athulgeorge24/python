f2f2c392d8762b979eb579bea7404b4895288811
athulgeorge24 <athulgeorge2028@cs.sjcetpalai.ac.in>
2024-10-18 20:05:31 +0530

    Add files via upload

---
+++ String_Specific.py
@@ -0,0 +1,3 @@
+
+
+print("Twinkle, twinkle,little star,\n\tHow i wonder what you are!,\n\t\t Up above the world so high,\n\t\t Like diamond in the sky,\nTwinkle twinkle little star,\n\tHow i wonder what yoy are" )
