61e759ad1155ed003f607483c6183ba871f8f7d0
athulgeorge24 <athulgeorge2028@cs.sjcetpalai.ac.in>
2024-10-18 15:07:59 +0530

    Add files via upload

---
+++ Extractstring.py
@@ -0,0 +1,14 @@
+'''
+Author:Athul George
+Date:18/10/24
+Discription: program that stores a string in a variable.
+Extract a specific part of the string (substring) and then concatenate it with another string.
+Finally, display the new string.
+'''
+
+str_var ="Hello, World!"
+str_var1=str_var[7:12]
+print(str_var1)
+str_var2=" Everyone!"
+str_var3=str_var1+str_var2
+print(str_var3)
