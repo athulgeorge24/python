74a24dffe936c80ecf7cff9560c2b71ab63aeb6d
athulgeorge24 <athulgeorge2028@cs.sjcetpalai.ac.in>
2024-10-19 14:38:31 +0530

    Add files via upload

---
+++ simple_calculator.py
@@ -0,0 +1,20 @@
+'''
+Author:Athul George
+Date:19/10/24
+Discription:Program to perform simple desktop calculator using Python
+'''
+num1=int(input("Enter a number:"))
+num2=int(input("Enter another number:"))
+num3=int(input("enter another number"))
+sum=num1+num2
+diff=num2-num1
+prod=num1*num2
+quot=num1/num2
+mod=num1%num2
+resu=(num1+num2)*num3/2
+print("The sum of num1 and num2 is",sum)
+print("The difference when num2 substracted from num1 is",diff)
+print("The product of num1 and num2 is:",prod)
+print("The quotient when num1 divided with num2 is:",quot)
+print("The reminder when num1 is divided with num2 is:",mod)
+print("The result of (num1+num2)*num3/2 is:",resu)
