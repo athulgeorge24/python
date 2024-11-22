82a9474093668a8f5e421ab770efa2e3c9c704c0
athulgeorge24 <athulgeorge2028@cs.sjcetpalai.ac.in>
2024-11-22 14:14:01 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,16 @@
+a=[12,32,43,37,17,16]
+b=[36,25,97,56,75,88]
+
+combine=a+b
+odd_emp=[]
+eve_emp=[]
+for i in combine:
+    if i%2==0:
+        eve_emp.append(i)
+    else:
+        odd_emp.append(i)
+odd_emp.sort()
+eve_emp.sort()
+print(combine)
+print(odd_emp)
+print(eve_emp)
