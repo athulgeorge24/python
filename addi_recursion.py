2c52f019adb89f5146c054ddb355836190bf53a3
athulgeorge24 <athulgeorge2028@cs.sjcetpalai.ac.in>
2024-11-29 20:49:30 +0530

    addi_recursion.py

--- addi_recursion.py
+++ addi_recursion.py
@@ -1,12 +1,14 @@
 num1 = int(input("Enter 1st number:"))
 num2 = int(input("Enter 2nd number:"))
-def addition(num1,num2):
-    if num1>0 and num2>0:
-        if num2==0:
-            return num1
-        else:
-            return addition(num1+1,num2-1)
-    else:
 
-y=addition(num1,num2)
-print("sum=",y)
+def addition(num1,num2):
+    if num2==0:
+        return num1
+    else:
+        return addition(num1+1,num2-1)
+
+if num1>0 and num2>0:
+    y=addition(num1,num2)
+    print("sum=",y)
+else:
+    print("enter valid positive numbers")
